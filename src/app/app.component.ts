7b8a17fbe9bff8d6f2dcb193eca0d78219087604
KarmAWalruS <yagin_v@mail.ru>
2023-09-03 08:06:07 +0300

    main page with faker API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Daum } from './components/main-board/main-board.interface';
+import { BooksService } from './components/services/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bulletin_board';
+
+  books: Daum[] = [];
+
+  constructor(private _booksService: BooksService) {}
+
+  ngOnInit() {
+    this._booksService.getBooks().subscribe((resp) => {
+      this.books = resp.data;
+    });
+  }
 }
